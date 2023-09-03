36ec2ea9da0e7685d06606c2743d130f5b8bd9a2
ABDUL MAJID K <majidmaji3410@gmail.com>
2023-09-03 10:57:14 +0530

    change the axios link url

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const axiosInstance = axios.create({
-    baseURL: 'https://paperplane-backend.onrender.com/api',
+    baseURL: 'https://paperplane-backend.onrender.com',
     headers: {
         'Content-Type': 'application/json'
     }
